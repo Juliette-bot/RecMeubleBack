935cab1ba3f011dbd335603d0ec7eea965b64f05
juliette <jblanc17@outlook.fr>
2025-11-05 16:36:54 +0100

    creation repo

---
+++ src/main/java/com/backend/recMeuble/RecMeubleApplication.java
@@ -0,0 +1,13 @@
+package com.backend.recMeuble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecMeubleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecMeubleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/recMeuble/RecMeubleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.recMeuble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecMeubleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
